a950d8b4cf524c4845b21fa9f24d31dc89c42e17
Daniel <danchan020@gmail.com>
2022-09-04 16:07:54 -0700

    resp and request

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-// import { ethers } from "ethers";
+import { ethers } from "ethers";
+import abi from "./utils/WavePortal.json";
 import './App.css';
 
 export default function App() {
@@ -7,6 +8,8 @@ export default function App() {
  //See if we can access the user's account
  
   const [currentAccount, setCurrentAccount] = useState("")
+  const contractAddress = "0x46974aeB793c2E3C4470B942cbE5B3458d284a38"
+  const contractABI = abi.abi
 
   const checkIfWallet = async() => {
     try{
@@ -54,6 +57,46 @@ const connectWallet = async() => {
   }
 }
 
+// call wave function from website
+
+const wave = async() => {
+  try{
+    const {ethereum} = window;
+    if (ethereum){
+      const provider = new ethers.providers.Web3Provider(ethereum);
+
+      // ethers is a library that helps frontend to talk to our contract, import it using import{ethers} from "ethers"
+
+      const signer = provider.getSigner();
+
+      // provider is what we use to talk to ethereum nodes
+
+      const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+      // passes in the contract address, contactABI: allows client to read data from smart contract, and wallet signer
+      // reading data is "free" (get request) and writing data needs to notify miners so txn can be mined (update request)
+      // changing the blockchain requires miners
+
+      let count = await wavePortalContract.getTotalWaves();
+      console.log("Retrieved total wave count...", count.toNumber());
+
+      const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        
+        console.log("Retrieved total wave count...", count.toNumber());
+    } else {
+      console.log("Ethereum object does not exist!");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
   useEffect(() => {
     checkIfWallet();
   }, [])
@@ -70,7 +113,7 @@ const connectWallet = async() => {
         Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" >
+        <button className="waveButton" onClick={wave} >
           Wave at Me
         </button>
         {!currentAccount && (
